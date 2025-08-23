257dfb7d71c825a3216c30867e14f0b3a64ad937
Illya Halych <miagreengal@gmail.com>
2025-08-23 14:32:37 +0300

    added some code for adding rooms in hotel and adding bookings for rooms

---
+++ book/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Room(models.Model):
+    number = models.IntegerField()
+    capacity = models.IntegerField()
+    location = models.TextField()
+
+    def __str__(self):
+        return f"Room #{self.number} (Capacity: {self.capacity})"
+
+    class Meta:
+        verbose_name = "Room"
+        verbose_name_plural = "Rooms"   
+        ordering = ['number']
+
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bookings')
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='bookings')
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    creation_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Booking by {self.user.username} for Room #{self.room.number} from {self.start_time} to {self.end_time}"
+
+    class Meta:
+        verbose_name = "Booking"
+        verbose_name_plural = "Bookings"
+        ordering = ['start_time']
